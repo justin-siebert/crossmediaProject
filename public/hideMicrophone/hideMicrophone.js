b3815681bc697c91095d5751b2de0dff4a9a56e9
JonahBolin <jonah.bolin17@gmail.com>
2026-05-13 23:49:56 +0200

    Uppdrag 7 funkar och finns

--- public/hideMicrophone/hideMicrophone.js
+++ public/hideMicrophone/hideMicrophone.js
@@ -1,5 +1,26 @@
+
+const clickMessage = document.createElement("p");
 const microphone = document.querySelector("#microphoneImg");
+const successText = document.createElement("p");
+const br = document.createElement("br");
+const phoneDiv = document.querySelector(".phone");
+
+
+successText.classList.add("textStyling");
+clickMessage.classList.add("textStyling");
+clickMessage.textContent = "Klicka på mikrofonen!";
+
+phoneDiv.prepend(clickMessage);
 
 microphone.addEventListener("click", () => {
     microphone.classList.add("moveIntoStone");
+
+    successText.textContent = "En riktig rackare är du! Är här de nästa koordinaterna som du behöver för att ta dig vidare:"
+    successText.appendChild(br);
+
+    successText.append("55.611321, 12.973578");
+
+    setTimeout(() => {
+        phoneDiv.appendChild(successText);
+    }, 1000);
 })
