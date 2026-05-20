0755d97de3f70546aa4a00624191db0954b2bfd9
Justin Siebert <19justinsiebert99@gmail.com>
2026-05-20 09:03:58 +0200

    css fixing

--- public/findPhone/phoneText.js
+++ public/findPhone/phoneText.js
@@ -1,4 +1,3 @@
-// --- Gemensamma variabler ---
 const pincode = ["1", "3", "1", "2"];
 let userChosenPin = [];
 let wrongs = 0;
@@ -99,7 +98,6 @@ async function typeOutSlowly(words) {
         await sleep(20);
     }
     
-    // Visa knappen (oavsett om den heter startBtn eller är en vanlig knapp)
     const btn = document.querySelector("button[onclick='startGame()']");
     if (btn) btn.style.display = "block";
 }
